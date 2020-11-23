77856aaf7c1277d1ff1146cf793ea1fde9bd33ae
Cazpar <casti19@student.sdu.dk>
2020-11-23 13:20:15 +0100

    Actuator

---
+++ src/Actuator/VentilationActuator.java
@@ -0,0 +1,13 @@
+apackage Actuator;
+
+public class VentilationActuator extends Actuator {
+    @Override
+    public void setValue(double value) {
+        super.setValue(value);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
