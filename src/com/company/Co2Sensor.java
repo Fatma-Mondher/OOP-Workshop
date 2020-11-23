221ec73ab06beaaa1aa58f1b64cdda17847f3dff
Oguz <oguz2863@gmail.com>
2020-11-23 12:57:02 +0100

    Implement Sensor

---
+++ src/com/company/Co2Sensor.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Co2Sensor extends Sensor{
+    @Override
+    public double getValue() {
+        return super.getValue();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
