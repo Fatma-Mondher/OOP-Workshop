137382b638e7e101b0f322fa288f8c3b88565d18
Cazpar <72437311+Cazpar@users.noreply.github.com>
2020-11-23 13:24:25 +0100

    Upload af Actuator

---
+++ src/com/company/Actuator.java
@@ -0,0 +1,21 @@
+package Actuator;
+
+public class Actuator extends Unit {
+    double value;
+    String building;
+
+    public void setBuilding(String building) {
+        this.building = building;
+    }
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public String getBuilding() {
+        return building;
+    }
+}
---
+++ src/com/company/VentilationActuator.java
@@ -0,0 +1,13 @@
+package Actuator;
+
+public class VentilationActuator extends Actuator {
+    @Override
+    public void setValue(double value) {
+        super.setValue(value);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
