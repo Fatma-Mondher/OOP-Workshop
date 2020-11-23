d7422c06d02a0dcfc049981e892c6a9bfffe2ba2
eeikr20 <71638709+eeikr20@users.noreply.github.com>
2020-11-23 12:51:08 +0100

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,30 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	//
+    class BuildingManagementSystem }
+    private Arraylist<building> building:
+    BuildingManagementSystem(ArrayList<building> building) {
+
+        this.items = items;
+    }
+    BuildingManagementSystem () {
+
+        this(new ArrayList<building>());
+    }
+    public void addBuilding (building building) {
+        if (!building.contains(building)) {
+            building.add(building);
+        }
+    }
+    public void removeBuilding (building building) {
+        items.remove(building);
+    }
+    public double getBuildingManagementSystem () {
+        double total = 0.0;
+        for (building building: building) {
+            total += building.getBuildingName();
+        }
+        return total;
     }
 }
