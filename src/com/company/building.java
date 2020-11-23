af25a232200fc6ec8c6824585f94186b25df5e74
eeikr20 <71638709+eeikr20@users.noreply.github.com>
2020-11-23 14:53:01 +0100

    Create building.java

---
+++ src/com/company/building.java
@@ -0,0 +1,32 @@
+package com.company;
+import java.util.HashMap;
+import java.util.ArrayList;
+public class building {
+    private String name;
+    private int   nr ;
+
+    building (String name, int nr) {
+        this.name = name;
+        this.nr  = nr;
+
+    }
+    public String getName () {
+        return name;
+    }
+    public int getNr() {
+        return nr;
+    }
+    public String toString () {
+        return "[Person name='"+name+"' nr='"+nr+"']";
+    }
+    public static void main (String args[]) { // subexercise 2
+        ArrayList<building> ps = new ArrayList<building>();
+        ps.add(new building("SDU" , 44));
+        ps.add(new building("OUH" , 2));
+        ps.add(new building("KU"  , 3));
+        ps.add(new building("AU", 4));
+        ps.add(new building("AAU", 5));
+
+
+}
+}
