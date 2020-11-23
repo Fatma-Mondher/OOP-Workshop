d5922fedeb52f5be4e4c94e8ab27ad3851d15464
Cazpar <72437311+Cazpar@users.noreply.github.com>
2020-11-23 14:10:53 +0100

    Update Actuator.java

--- src/com/company/Actuator.java
+++ src/com/company/Actuator.java
@@ -4,20 +4,14 @@ import com.company.Unit;
 
 public class Actuator extends Unit {
     double value;
-    String building;
 
-    public void setBuilding(String building) {
-        this.building = building;
+    private void Actuator (double value) {
+        this.value = value
     }
     public void setValue(double value) {
         this.value = value;
     }
-
     public double getValue() {
         return value;
-    }
-
-    public String getBuilding() {
-        return building;
     }
 }
